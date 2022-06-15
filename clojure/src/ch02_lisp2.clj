956a88ea38c78a0ecd24547d435649a55b7811aa
Juraj Martinka <jumarko@gmail.com>
2022-06-15 15:14:57 +0200

    LiSP ch2 - update code for section 2.5.2 - comments, keyword update, example update.

--- clojure/src/ch02_lisp2.clj
+++ clojure/src/ch02_lisp2.clj
@@ -688,13 +688,12 @@
 
 
 
-;;; 2.5.2 Dynamic vars in common lisp
-;;;
+;;; 2.5.2 Dynamic vars in common lisp (p. 48-50)
 ;; we can ignore this function for now - it's not even defined in the book
 (defn cl-update! [id env denv value]
   ;; this is just copied from `e/update!`
   (if (map? env)
-    ;; TODO: here we would update both environments? (mutation?)
+    ;; TODO: here we should update both environments? (mutation?)
     (assoc env id value)
     (e/wrong "Can't understand environment" env {:id id
                                                  :env env
@@ -703,24 +702,37 @@
 (defn special-extend [env variables]
   ;; TODO: What if there's a lexical variable already in env
   ;; with the same name as one of `variables`?
-  (merge env (zipmap variables (repeat ::dynamic))))
+  (merge env (zipmap variables
+                     ;; we use special keyword as a value of dynamic vars
+                     ;; to distinguish them from non-dynamic vars
+                     ;; these dynamic vars are then looked up in `denv` inside `cl-lookup`
+                     (repeat :lisp/dynamic.var))))
 
 (special-extend {'a 1 'b 2}
                 ['c 'd])
-;; => {a 1, b 2, c :ch02-lisp2/dynamic, d :ch02-lisp2/dynamic}
+;; => {a 1, b 2, c :lisp/dynamic.var, d :lisp/dynamic.var}
 
-(defn cl-lookup [id env denv]
+(defn cl-lookup [var env denv]
   (if (map? env)
-    (if-let [[_ var] (find env id)]
-      (if (= var :ch02-lisp2/dynamic)
-        (e/lookup id denv)
-        var))
-    (e/wrong "Can't understand environment" env {:id id
+    ;; all the known symbols should be in `env`...
+    (if-let [[_ value] (find env var)]
+      ;; ... but dynamic vars have only a placeholder value
+      ;; and their real value must be looked up in `denv`
+      ;; - see also `special-extend`
+      (if (= value :lisp/dynamic.var)
+        ;; Notice that unlike in the book (p.49) we do not fallback to `e/env-global`
+        ;; - not sure why they did it
+        (e/lookup var denv)
+        value)
+      (e/wrong "No such binding" var))
+    (e/wrong "Can't understand environment" env {:id var
                                                  :env env
                                                  :denv denv})))
 
 (def test-env (special-extend {'a 1 'b 2}
                               ['c 'd]))
+test-env
+;; => {a 1, b 2, c :lisp/dynamic.var, d :lisp/dynamic.var}
 
 (cl-lookup 'c test-env {'c 1})
 ;; => 1
@@ -788,12 +800,19 @@
 (df-evaluate '(dynamic-let ((x 2)) x) {} {} {'x 4})
 ;; => 2
 
-(df-evaluate '(dynamic-let ((x 3))
-                           ( (lambda (x)
-                                        (+ x (dynamic x)))
-                            (+ x x) ))
-             {}
-             {'+ (fn [args _denv] (apply + args))}
-             {'x 2})
-;; => 9
-;; => 6
+;; this is the example at the end of 2.5.2 (p.50)
+;; we use `lambda` instead of `let` because let isn't supported in our language
+(assert
+ (= 13
+    (df-evaluate '(dynamic-let ((x 2))
+                               (+ x ; dynamic va: 2
+                                  ;; `lambda` serves us as `let`
+                                  ((lambda (x) ; lexical var: 9
+                                           (+ x ; lexical var: 9
+                                              (dynamic x) ; dynamic var: 2
+                                              ))
+                                   (+ x x 5) ; dynamic var - value of this exp. will become the lexical var in lambda's body
+                                   )))
+                 {}
+                 {'+ (fn [args _denv] (apply + args))}
+                 {'x 20}))) ; sort of 'global' dynamic var - it's value is never used
